02f00a2bd6e6d87e1455db4182c70162ca29eb11
Vijeshnk <vijeshnirala@gmail.com>
2024-02-20 21:54:24 +0530

    added the comment and change in url endpoint

--- app/components/ResizableComponent/ResizableComponent.tsx
+++ app/components/ResizableComponent/ResizableComponent.tsx
@@ -3,9 +3,15 @@ import { Resizable } from 're-resizable';
 import axios from 'axios';
 import 'tailwindcss/tailwind.css';
 
+interface Element {
+    _id: string;
+    content: string;
+}
+
+
 export const ResizableComponent = () => {
     const [data, setData] = useState('');
-    const [elements, setElements] = useState([]);
+    const [elements, setElements] = useState<Element[]>([]);
     const [selectedElementId, setSelectedElementId] = useState('');
     const [operationCount, setOperationCount] = useState({ adds: 0, updates: 0 });
 
@@ -14,6 +20,9 @@ export const ResizableComponent = () => {
         fetchOperationCounts();
     }, []);
 
+    /**
+     * The function fetches elements from local storage and sets them in the state if they exist.
+     */
     const fetchElementsFromLocalStorage = () => {
         const storedElements = localStorage.getItem('elements');
         if (storedElements) {
@@ -21,23 +30,40 @@ export const ResizableComponent = () => {
         }
     };
 
-    const saveElementToLocalStorage = (newElement) => {
+    /**
+     * The function `saveElementToLocalStorage` saves a new element to local storage and fetches all
+     * elements from local storage.
+     * @param {Element} newElement - `newElement` is the element that needs to be saved to the local
+     * storage.
+     */
+    const saveElementToLocalStorage = (newElement: Element) => {
         const updatedElements = [...elements, newElement];
         localStorage.setItem('elements', JSON.stringify(updatedElements));
-        fetchElementsFromLocalStorage(); 
+        fetchElementsFromLocalStorage();
     };
 
-    const updateElementInLocalStorage = (updatedElement) => {
+    /**
+     * The function `updateElementInLocalStorage` updates a specific element in local storage and fetches
+     * the updated elements.
+     * @param {Element} updatedElement - The `updatedElement` parameter in the
+     * `updateElementInLocalStorage` function represents the element that has been updated and needs to
+     * be saved in the local storage.
+     */
+    const updateElementInLocalStorage = (updatedElement: Element) => {
         const updatedElements = elements.map((element) =>
             element._id === updatedElement._id ? updatedElement : element
         );
         localStorage.setItem('elements', JSON.stringify(updatedElements));
-        fetchElementsFromLocalStorage(); 
+        fetchElementsFromLocalStorage();
     };
 
+  /**
+   * The function fetchOperationCounts fetches operation counts from a specified API endpoint and
+   * updates the state with the total number of adds and updates.
+   */
     const fetchOperationCounts = async () => {
         try {
-            const response = await axios.get('http://localhost:8080/data/count');
+            const response = await axios.get('https://node-mongo-api-qff7.onrender.com/data/count');
             setOperationCount({
                 adds: response.data[0].totalAdds,
                 updates: response.data[0].totalUpdates
@@ -47,9 +73,13 @@ export const ResizableComponent = () => {
         }
     };
 
+  /**
+   * The handleAdd function sends a POST request to a server, saves the response to local storage,
+   * clears the input data, and fetches updated operation counts.
+   */
     const handleAdd = async () => {
         try {
-            const response = await axios.post('http://localhost:8080/data', { content: data });
+            const response = await axios.post('https://node-mongo-api-qff7.onrender.com/data', { content: data });
             const newElement = { ...response.data, content: data };
             saveElementToLocalStorage(newElement);
             setData('');
@@ -59,16 +89,22 @@ export const ResizableComponent = () => {
         }
     };
 
+ /**
+  * The `handleUpdate` function updates data for a selected element using an HTTP PUT request and
+  * handles error cases.
+  * @returns The `handleUpdate` function returns nothing explicitly, but it may return a promise if the
+  * `axios.put` call is made asynchronously.
+  */
     const handleUpdate = async () => {
         if (!selectedElementId) {
             alert('Please select an element to update.');
             return;
         }
         try {
-            await axios.put(`http://localhost:8080/data/${selectedElementId}`, { content: data });
+            await axios.put(`https://node-mongo-api-qff7.onrender.com/data/${selectedElementId}`, { content: data });
             const updatedElement = { _id: selectedElementId, content: data };
             updateElementInLocalStorage(updatedElement);
-            fetchOperationCounts(); 
+            fetchOperationCounts();
             setData('');
             setSelectedElementId('');
         } catch (error) {
@@ -76,7 +112,13 @@ export const ResizableComponent = () => {
         }
     };
 
-    const selectElement = (element) => {
+   /**
+    * The `selectElement` function sets data and the selected element ID based on the input element.
+    * @param {Element} element - The `element` parameter in the `selectElement` function is of type
+    * `Element`. It is an object that represents an element in the DOM (Document Object Model) with
+    * properties such as `content` and `_id`.
+    */
+    const selectElement = (element: Element) => {
         setData(element.content);
         setSelectedElementId(element._id);
     };
@@ -97,7 +139,7 @@ export const ResizableComponent = () => {
                 }}
                 className="bg-orange-100"
             >
-            
+
                 <textarea
                     className="w-full h-24 p-2 mb-4"
                     value={data}
